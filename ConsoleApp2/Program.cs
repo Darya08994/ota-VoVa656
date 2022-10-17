377ec4047d98aaa146e345d7728168ce795406e6
Darya08994 <115993035+Darya08994@users.noreply.github.com>
2022-10-17 14:40:31 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("год:" + "2019, 2020, 2021, 2022 ");
+            char n = char.Parse(Console.ReadLine());
+            switch (n)
+            {
+                case (char)2019 :
+                    Console.WriteLine("Год какого животного: ");
+                    Console.Write("Животное= ");
+                    double a = double.Parse(Console.ReadLine());
+                    {
+                        Console.WriteLine("p={Желтая свинья}");
+                    }
+                    break;
+                case (char)2020:
+                    Console.WriteLine("Год какого животного");
+                    Console.Write("Животное= ");
+                    double b = double.Parse(Console.ReadLine());
+                    {
+                        Console.WriteLine("p={Белая крыса}");
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ ConsoleApp5 Жмакина ИС-21-т2/ConsoleApp1 Жмакина/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1_Жмакина
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         string name;
+        System.Console.WriteLine("Введите ваше имя: ");
+        name = System.Console.ReadLine();
+        System.Console.WriteLine("Приветствую Вас, " + name + "!");
+       }
+    }
+}
